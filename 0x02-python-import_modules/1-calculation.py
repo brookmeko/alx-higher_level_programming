aac458e77dcb4ee75e40d8feb73ef0782e59e86e
root <brookmeko42@gmail.com>
2022-12-09 06:06:05 +0000

    cal

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-# 1_calculator
+# 1-calculation.py
 # brook
 
 if __name__ == "__main__":
-	"""print the sum div mult and submissio"""
-	from calculator_1 import add, sub, mul, div
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
 
-	a = 10
-	b = 5
+    a = 10
+    b = 5
 
-	print("{} + {} = {}".format(a, b, add(a,b)))
-	print("{} - {} = {}".format(a, b, sub(a,b)))
-	print("{} * {} = {}".format(a, b, mul(a,b)))
-	print("{} / {} = {}".format(a, b, div(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
