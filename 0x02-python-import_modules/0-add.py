e2cdc86f778aee4d827f02c2f4e7f59ec51a9a36
root <brookmeko42@gmail.com>
2022-12-09 04:56:40 +0000

    import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
-	return (a + b)
-	
-print(add(1, 2))
+# 0_add.py
+# Brook 
+
+if __name__ == "__main__":
+	"""print the sum of 1 and 2."""
+	from add_0 import add
+
+	a = 1
+	b = 2
+	print("{} + {} = {}".format(a, b, add(a, b)))
