44f2a0f006067473741fea35a555104803cc223f
brookmeko <birukmeko42@gmail.com>
2022-12-21 04:42:30 -0800

    print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,8 @@
 for num in range(0, 10):
   for num1 in range (0, 10):
     if num < num1:
-      print('{}{}'.format(num, num1), end=" ")
+        if not (num == 8 and num1 == 9):
+            print('{}{}'.format(num, num1), end=" ")
+        else:
+            print('{}{}'.format(num, num1), end=" ")
+print('')
