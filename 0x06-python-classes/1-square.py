7c68a31a809a7a4f198c18277958ecd44d52257d
root <brookmeko42@gmail.com>
2023-01-13 06:05:59 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class of square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def__init__(self, size):
+     """intitalization of new square
+	
+	args:
+		size (init) the siez
+	"""
+        self.__size = size
