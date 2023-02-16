1f114021db6115fbff94e4dd5adaabd9626ce60d
root <brookmeko42@gmail.com>
2023-02-16 13:40:01 +0000

    insert

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- adding new row in the table
-INSERT INTO first_table(id, name) VALUES(89, Best School)
+-- nserts a new row in the table first_table
+INSERT INTO first_table(id, name) VALUES(89, 'Best School');
