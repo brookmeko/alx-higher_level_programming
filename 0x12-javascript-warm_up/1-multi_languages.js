d820c8f49f41ffebe6b6cd610787619333d602c4
root <brookmeko42@gmail.com>
2023-03-13 14:44:40 +0000

    mult

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
+// It prints 3 lines
 
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(myVar);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
